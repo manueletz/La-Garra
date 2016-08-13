c79030637885fee5f70e32b9353626c2468b3526
Manuel Estevez <manueletz@hotmail.com>
2016-08-12 18:14:57 -0600

    Version 4

---
+++ Assets/Instanciar.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instanciar : MonoBehaviour {
+
+	public GameObject premio1, premio2, premio3;
+	public GameObject prefab;
+	public float myNumeroAleatorio=-1.0F;
+	public bool fin=false;
+
+	public GameObject[] mispremios;
+
+	public Sprite[] faces;
+
+	public GameObject frutas;
+
+
+	// Use this for initialization
+	void Start () {
+		//premio1 = GameObject.Find ("Premios").GetComponent<Transform>().FindChild("bomb");
+		premio1 = GameObject.Find("bomb");
+		premio2 = GameObject.Find("clover");
+		premio3 = GameObject.Find("bronze_coin");
+
+
+
+
+			mispremios = GameObject.FindGameObjectsWithTag("TagPremios");
+
+			for(int i = 0; i < mispremios.Length; i++)
+			{
+				Debug.Log("Premio Number "+i+" is named "+mispremios[i].name);
+			}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		EmitirPremio ();
+		//GameObject premio1Copy = Instantiate (premio1);
+	}
+
+	void EmitirPremio(){
+		/* esta funciona correctamente
+		if (Random.value < 1f /(60f * 3f)){
+			GameObject goldCopy = Instantiate (goldPrototype);
+			GameObject rubyCopy = Instantiate (rubyPrototype);
+			GameObject diamondCopy = Instantiate (diamondPrototype);
+			goldCopy.transform.position = new Vector3(-0.33f,0.99f,0f);
+		}
+		*/
+		myNumeroAleatorio = Random.Range(0F, 1000F);
+
+
+		//switch (myNumeroAleatorio){
+		if (myNumeroAleatorio>=0 && myNumeroAleatorio<1 && fin==false){
+			GameObject frutasCopy = Instantiate(frutas);
+			frutasCopy.GetComponent<SpriteRenderer> ().enabled = true;
+			//GameObject premio1Copy = Instantiate (premio1);
+			//premio1Copy.GetComponent<SpriteRenderer> ().enabled = true;
+			//GameObject goldCopy = Instantiate (goldProototype);
+			//goldCopy.transform.position = new Vector3(-0.33f,3.83f,0f);
+		}
+		if (myNumeroAleatorio>=30 && myNumeroAleatorio<31 && fin==false){
+			GameObject frutasCopy = Instantiate(frutas);
+			frutasCopy.GetComponent<SpriteRenderer> ().enabled = true;
+			//GameObject premio2Copy = Instantiate (premio2);
+			//premio2Copy.GetComponent<SpriteRenderer> ().enabled = true;
+			//GameObject rubyCopy = Instantiate (rubyPrototype);
+			//rubyCopy.transform.position = new Vector3(-0.33f,3.83f,0f);
+		}
+		if (myNumeroAleatorio>=60 && myNumeroAleatorio<=61 && fin==false){
+			GameObject frutasCopy = Instantiate(frutas);
+			frutasCopy.GetComponent<SpriteRenderer> ().enabled = true;
+			//GameObject premio3Copy = Instantiate (premio3);
+			//premio3Copy.GetComponent<SpriteRenderer> ().enabled = true;
+			//GameObject diamondCopy = Instantiate (diamondPrototype);
+			//diamondCopy.transform.position = new Vector3(-0.33f,3.83f,0f);
+		}
+
+	}
+}
---
+++ Assets/controlPremios.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controlPremios : MonoBehaviour {
+
+	AudioSource aSource;
+	public AudioClip ganarPremioSound;
+	GameObject premio=null;
+
+	Rigidbody2D rgb;
+	SpriteRenderer sr;
+	public float maxVel = 10f;
+
+	public int girarx = 0;
+	public int girary = 0;
+	public int girarz = -5;
+
+	bool ActivarRotacion = false;
+
+	// Use this for initialization
+	void Start () {
+		premio = GameObject.Find ("cake_source_53");
+		rgb = GetComponent<Rigidbody2D> ();
+		sr = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		aSource = GetComponent<AudioSource> ();
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			//Se genera fuerza sobre la bala, lanzándola en dirección del eje z de la bola (el frente de la bola)
+			//rigidbodyObjeto.AddForce (transform.forward * fuerza);
+			float fuerza = 2700f;
+			rgb.AddForce (transform.up * fuerza);
+		}
+
+		if (ActivarRotacion==true) {
+//			premio.GetComponent<Transform> ().Rotate (girarx, girary, girarz);
+		}
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		//Debug.Log(sr.sprite.name.Equals
+		//if (other.gameObject.name.Equals ("cake_source_53") && (sr.sprite.name.Equals("resortes_4") || (sr.sprite.name.Equals("resortes_3") ))) {
+		//if (other.gameObject.name.Equals ("cake_source_53")	&& (sr.sprite.name.Contains("resortes"))) {
+		//if ( (other.gameObject.name.Equals ("cake_source_53") || other.gameObject.name.Contains ("bomb") || other.gameObject.name.Contains ("clover") || other.gameObject.name.Contains ("bronze_coin")	)&& (sr.sprite.name.Contains("resortes"))) {
+		if ( (other.gameObject.name.Contains ("FrutasPrototype") || other.gameObject.name.Contains ("bomb") || other.gameObject.name.Contains ("clover") || other.gameObject.name.Contains ("bronze_coin")	)&& (sr.sprite.name.Contains("resortes"))) {
+			Debug.Log ("Toque la fruta");
+			aSource.PlayOneShot (ganarPremioSound);
+			//premio.GetComponent<Salto>().activo=false;
+			//other.gameObject.GetComponent<Salto>().activo=false;
+
+			Destroy (other.gameObject);
+
+			//float v = Input.GetAxis ("Vertical");
+			//Vector2 vel = new Vector2 (rgb.velocity.x, 0);
+			//v *= maxVel;
+			//vel.y = v;
+			//float power = 10000f;
+			//premio.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up*power);
+			//float speed = 10000f;
+			//premio.GetComponent<Rigidbody2D>().AddForce(transform.up*Time.deltaTime*speed);
+			float fuerza = 2700f;
+//			premio.GetComponent<Rigidbody2D> ().AddForce (transform.up * fuerza);
+			ActivarRotacion = true;
+			//Destroy (premio);
+		}
+
+	}
+
+	void FixedUpdate () {
+		// Cambio de los metodos para verificar el input
+		VerificarInputParaCaminar();
+	}
+
+	private void VerificarInputParaCaminar() {
+		float v = Input.GetAxis ("Horizontal");
+		Vector2 vel = new Vector2 (0, rgb.velocity.y);
+		v *= maxVel;
+		vel.x = v;
+		rgb.velocity = vel;
+	}
+}
